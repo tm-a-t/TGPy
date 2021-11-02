fc0e66d0fb32e0a9b632f5bfcd0de6435091a620
Anton Egorov <a12286@yandex.com>
2021-11-02 16:55:34 +0300

    refactor: app/run_code/parse_code.py: shorter lines, better function names

--- app/run_code/parse_code.py
+++ app/run_code/parse_code.py
@@ -6,32 +6,38 @@ class _Result:
     uses_orig = False
 
 
-def _is_node_fp_word(node: ast.AST, locs: dict) -> bool:
+def _is_node_unknown_variable(node: ast.AST, locs: dict) -> bool:
     """Check if AST node is a Name or Attribute not present in locals"""
     if isinstance(node, ast.Attribute) and isinstance(node.value, ast.Name):
         return node.value.id not in locs
     return isinstance(node, ast.Name) and node.id not in locs
 
 
-def _check_node_on_false_binop(node: ast.AST, locs: dict) -> bool:
-    """Check if AST node can be an operand or binary operation (ast.BinOp, ast.Compare, ast.BoolOp)
-    with operands which do not pass _is_node_fp_word check"""
-    if _is_node_fp_word(node, locs):
+def _is_node_suspicious_binop(node: ast.AST, locs: dict) -> bool:
+    """Check if AST node can be an operand of binary operation (ast.BinOp, ast.Compare, ast.BoolOp)
+    with operands which do not pass _is_node_unknown_variable check, or is such operation"""
+    if _is_node_unknown_variable(node, locs):
         return True
     if not isinstance(node, (ast.BoolOp, ast.BinOp, ast.Compare)):
         return False
     if isinstance(node, ast.Compare):
-        return _is_node_fp_word(node.left, locs) and all(_is_node_fp_word(x, locs) for x in node.comparators)
-    return all(_check_node_on_false_binop(operand, locs) for operand in ((node.left, node.right) if isinstance(node, ast.BinOp) else node.values))
+        return _is_node_unknown_variable(node.left, locs) and all(_is_node_unknown_variable(x, locs)
+                                                                  for x in node.comparators)
+    return all(_is_node_suspicious_binop(operand, locs)
+               for operand in ((node.left, node.right) if isinstance(node, ast.BinOp) else node.values))
 
 
-def _is_node_false(node: ast.AST, locs: dict) -> bool:
+def _ignore_node(node: ast.AST, locs: dict) -> bool:
     """Check if AST node didn't seem to be meant to be code"""
     return (
-        isinstance(node, ast.Constant) or _is_node_fp_word(node, locs)
-        or isinstance(node, ast.UnaryOp) and isinstance(node.operand, (ast.Constant, ast.Name, ast.Attribute))  # Messages like "-1", "+spam" and "not foo.bar"
-        or _check_node_on_false_binop(node, locs)  # Messages like one-two, one is two, one >= two, one.b in two.c
-        or isinstance(node, ast.Tuple) and all(_is_node_false(elt, locs) for elt in node.elts)  # "(yes, understood)"
+            # Messages like "python", "123" or "example.com"
+            isinstance(node, ast.Constant) or _is_node_unknown_variable(node, locs)
+            # Messages like "-1", "+spam" and "not foo.bar"
+            or isinstance(node, ast.UnaryOp) and isinstance(node.operand, (ast.Constant, ast.Name, ast.Attribute))
+            # Messages like one-two, one is two, one >= two, one.b in two.c
+            or _is_node_suspicious_binop(node, locs)
+            # Messages like "yes, understood"
+            or isinstance(node, ast.Tuple) and all(_ignore_node(elt, locs) for elt in node.elts)
     )
 
 
@@ -44,7 +50,7 @@ def parse_code(text: str, locs: dict) -> _Result:
     except (SyntaxError, ValueError):
         return result
 
-    if all(isinstance(body_item, ast.Expr) and _is_node_false(body_item.value, locs) for body_item in root.body):
+    if all(isinstance(body_item, ast.Expr) and _ignore_node(body_item.value, locs) for body_item in root.body):
         return result
 
     result.is_code = True
