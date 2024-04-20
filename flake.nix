8686aa0c8bec91a1587deac32af5dcb442da8f2c
Ivan Filipenkov <hello@vanutp.dev>
2024-04-20 21:31:38 +0200

    ci(guide): add guide dependencies to pyproject.toml, build guide using nix

--- flake.nix
+++ flake.nix
@@ -10,33 +10,42 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        buildTgpy = (pkgs: 
-          let 
-           inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-          in builtins.trace "Not all features are supported due to the nature of nix" mkPoetryApplication rec { 
-            projectDir = ./.; 
-            preferWheels = true;
-          }
-        );
+        pkgs = import nixpkgs { inherit system; };
+        p2n = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
 
-        buildTgpyImage = (pkgs: pkgs.dockerTools.buildLayeredImage {
-          name = "tgpy_image";
-          contents = [ (buildTgpy pkgs) ];
-          created = "now";
-          config = {
-            Cmd = [ "tgpy" ];
+        buildTgpy =
+          pkgs:
+          dev:
+          builtins.trace "Not all features are supported due to the nature of nix" p2n.mkPoetryApplication {
+            projectDir = ./.;
+            preferWheels = true;
+            groups = if dev then [ "dev" "guide" ] else [];
           };
-        });
+
+        buildTgpyImage =
+          pkgs:
+          pkgs.dockerTools.buildLayeredImage {
+            name = "tgpy_image";
+            contents = [ (buildTgpy pkgs false) ];
+            created = "now";
+            config = {
+              Cmd = [ "tgpy" ];
+            };
+          };
       in
       {
         packages = {
-          tgpy = buildTgpy pkgs;      
+          tgpy = buildTgpy pkgs false;
           tgpyImage = buildTgpyImage pkgs;
           tgpyImage-aarch64Linux = buildTgpyImage pkgs.pkgsCross.aarch64-multiplatform;
           tgpyImage-x86_64Linux = buildTgpyImage pkgs.pkgsCross.gnu64;
@@ -44,8 +53,9 @@
         };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.tgpy ];
+          inputsFrom = [ (buildTgpy pkgs true) ];
           packages = [ pkgs.poetry ];
         };
-      });
+      }
+    );
 }
