a2298c88388785e764e1f30b977d6d6ad2ca131f
Gleb Smirnov <glebsmirnov0708@gmail.com>
2024-09-15 11:14:07 +0300

    nix: rework flake to make it simple and convenient

--- flake.nix
+++ flake.nix
@@ -11,51 +11,42 @@
   };
 
   outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }@inputs:
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      poetry2nix,
-    }:
+      lib.mkTgpy =
+        { system ? null
+        , pkgs ? import nixpkgs { inherit system; }
+        , poetry2nix ? inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }
+        }: poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+    } //
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        p2n = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
-
-        buildTgpy =
-          pkgs:
-          dev:
-          builtins.trace "Not all features are supported due to the nature of nix" p2n.mkPoetryApplication {
-            projectDir = ./.;
-            preferWheels = true;
-            groups = if dev then [ "dev" "guide" ] else [];
-          };
-
-        buildTgpyImage =
-          pkgs:
-          pkgs.dockerTools.buildLayeredImage {
-            name = "tgpy_image";
-            contents = [ (buildTgpy pkgs false) ];
-            created = "now";
-            config = {
-              Cmd = [ "tgpy" ];
-            };
-          };
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
       in
       {
-        packages = {
-          tgpy = buildTgpy pkgs false;
-          tgpyImage = buildTgpyImage pkgs;
-          tgpyImage-aarch64Linux = buildTgpyImage pkgs.pkgsCross.aarch64-multiplatform;
-          tgpyImage-x86_64Linux = buildTgpyImage pkgs.pkgsCross.gnu64;
-          default = self.packages.${system}.tgpy;
+        packages = rec {
+          tgpy = self.lib.mkTgpy { inherit system; };
+          default = tgpy;
         };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ (buildTgpy pkgs true) ];
-          packages = [ pkgs.poetry ];
-        };
+        devShells.default = (poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          preferWheels = true;
+        }).overrideAttrs (old: {
+          nativeBuildInputs = with pkgs; [
+            poetry
+            python3Packages.python-lsp-server
+          ];
+        });
       }
     );
 }
