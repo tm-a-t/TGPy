fb03e3485b25f439074db1e0a5612d7d95798938
Stanislav Alekseev <stas.ale66@gmail.com>
2024-09-21 19:18:18 +0300

    nix: build with preferWheels = false (#45)

---
+++ nix/nh3overrides.nix
@@ -0,0 +1,17 @@
+{ super, pkgs, ... }:
+super.nh3.overridePythonAttrs (old: {
+  cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+    inherit (old) src;
+    name = "${old.pname}-${old.version}";
+    sha256 = "sha256-fetAE3cj9hh4SoPE72Bqco5ytUMiDqbazeS2MHdUibM=";
+  };
+  buildInputs = old.buildInputs or [ ] ++ (pkgs.lib.optionals super.stdenv.isDarwin [
+    pkgs.libiconv
+  ]);
+  nativeBuildInputs = old.nativeBuildInputs or [ ] ++ (with pkgs.rustPlatform; [
+    cargoSetupHook
+    maturinBuildHook
+    pkgs.cargo
+    pkgs.rustc
+  ]);
+})
