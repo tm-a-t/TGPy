0fcac585e148b37514f5a678e2997c64c91998d8
Ivan Filipenkov <hello@vanutp.dev>
2025-04-05 04:16:39 +0200

    feat: add star import fix to std

---
+++ tgpy/std/star_imports.py
@@ -0,0 +1,42 @@
+"""
+name: star_imports
+origin: tgpy://builtin_module/star_imports
+priority: 300
+"""
+
+import ast
+
+import tgpy.api
+
+
+def unwrap_star_import(module_name: str) -> list[str]:
+    # https://stackoverflow.com/a/41991139
+    module = __import__(module_name, fromlist=['*'])
+    if hasattr(module, '__all__'):
+        names = module.__all__
+    else:
+        names = [name for name in dir(module) if not name.startswith('_')]
+    return names
+
+
+class StarImportsTransformer(ast.NodeTransformer):
+    def visit_ImportFrom(self, node: ast.ImportFrom):
+        node = self.generic_visit(node)
+        if node.names[0].name == '*':
+            try:
+                # this has a downside of delaying the start
+                # of message evaluation if the import takes a long time
+                names = unwrap_star_import(node.module)
+                node.names = [ast.alias(name) for name in names]
+            except ImportError:
+                pass
+        return node
+
+
+def ast_transformer(tree: ast.AST) -> ast.AST:
+    return StarImportsTransformer().visit(tree)
+
+
+tgpy.api.ast_transformers.add('star_imports', ast_transformer)
+
+__all__ = []
