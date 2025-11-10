f66d124b1ea341c854979c740a83eeff587c0795
Gleb Smirnov <glebsmirnov0708@gmail.com>
2025-11-10 21:36:23 +0300

    build: update some dependencies
    - update `rich` to 14.0
    - stop using downstream version of `telethon`

--- nix/mkPackageOverrides.nix
+++ nix/mkPackageOverrides.nix
@@ -1,16 +1,5 @@
-{ pkgs }:
+{ ... }:
 self: super: {
-  telethon-v1-24 = super.telethon.overrideAttrs (old: rec {
-    version = "1.24.21";
-    pname = "telethon_v1_24";
-    src = pkgs.fetchPypi {
-      inherit version pname;
-      hash = "sha256-/RIWWawU4lp/fYmHq0KOU17XmllNSMos9pOb7A9FhHA=";
-    };
-    patches = [ ];
-    doCheck = false;
-    pytestCheckPhase = "true";
-  });
   mkdocs-git-revision-date-localized-plugin =
     super.mkdocs-git-revision-date-localized-plugin.overrideAttrs
       (old: {
