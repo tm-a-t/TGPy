da030c75a3ebfdbaefaebea284bc46465c22f232
Elk Cloner <dev@elkcl.ru>
2026-04-07 23:35:36 +0300

    feat: pin telethon separately in nix overrides

--- nix/mkPackageOverrides.nix
+++ nix/mkPackageOverrides.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 self: super: {
   mkdocs-git-revision-date-localized-plugin =
     super.mkdocs-git-revision-date-localized-plugin.overrideAttrs
@@ -7,5 +7,15 @@ self: super: {
         format = null;
 
         dependencies = old.propagatedBuildInputs ++ [ super.setuptools-scm ];
+      });
+  telethon =
+    super.telethon.overridePythonAttrs
+      (_: {
+        src = pkgs.fetchFromCodeberg {
+          owner = "Lonami";
+          repo = "Telethon";
+          rev = "09ef697621aac7cf9e80b538063b2ca378eb2997";
+          hash = "sha256-m55X17mIHmInDun+0685fWWFdPySt/4Ar4z+gl6blek=";
+        };
       });
 }
