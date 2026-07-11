dcc35f86747f86d4a9e0e3a795c2dee8244439db
Ivan Filipenkov <fox@vanutp.dev>
2026-07-11 13:30:58 +0200

    build(docker): remove redundant PYTHONPATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 if [ ! -f container_setup_completed ]; then
-  PYTHONPATH=. python /app/docker/install_mods.py
+  python /app/docker/install_mods.py
   touch container_setup_completed
 fi
 
