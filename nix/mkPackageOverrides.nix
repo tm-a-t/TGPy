f15b60f0ac1c2fde348b56fa0865d0dcbcd70ad7
Ivan Filipenkov <fox@vanutp.dev>
2026-08-25 17:19:21 +0200

    feat: update telethon (layer 229)

--- nix/mkPackageOverrides.nix
+++ nix/mkPackageOverrides.nix
@@ -13,8 +13,8 @@ self: super: {
     src = pkgs.fetchFromCodeberg {
       owner = "Lonami";
       repo = "Telethon";
-      rev = "5498ab9a9edb37cb1e86e838cca4f1f5ba611371";
-      hash = "sha256-JBemqYi6t868945C+eGOdBE8Z+SM/iJHJK+gI/GNFmM=";
+      rev = "7df274b72e489eab74895a8c574b865873170411";
+      hash = "sha256-XAGE0pQyWDmrdAOrJUTE7DM/PyYhV+3lmhfTThh3ZSw=";
     };
     build-system = [ super.hatchling ];
     patches = [ ];
