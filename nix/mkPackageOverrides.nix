53ea0d1a9164a9ff5769e7717c5b8e23cb651ff8
Ivan Filipenkov <fox@vanutp.dev>
2026-08-27 16:51:28 +0200

    build(nix): fix build

--- nix/mkPackageOverrides.nix
+++ nix/mkPackageOverrides.nix
@@ -8,16 +8,26 @@ self: super: {
 
         dependencies = old.propagatedBuildInputs ++ [ super.setuptools-scm ];
       });
-  telethon = super.telethon.overridePythonAttrs {
-    version = "1.44.0-dev";
-    src = pkgs.fetchFromCodeberg {
-      owner = "Lonami";
-      repo = "Telethon";
-      rev = "7df274b72e489eab74895a8c574b865873170411";
-      hash = "sha256-XAGE0pQyWDmrdAOrJUTE7DM/PyYhV+3lmhfTThh3ZSw=";
-    };
-    build-system = [ super.hatchling ];
-    patches = [ ];
-    nativeCheckInputs = [ ];
-  };
+  telethon = super.telethon.overridePythonAttrs (
+    old:
+    let
+      version = "1.44.1.dev1";
+    in
+    {
+      inherit version;
+      src = pkgs.fetchFromCodeberg {
+        owner = "Lonami";
+        repo = "Telethon";
+        rev = "7df274b72e489eab74895a8c574b865873170411";
+        hash = "sha256-XAGE0pQyWDmrdAOrJUTE7DM/PyYhV+3lmhfTThh3ZSw=";
+      };
+      build-system = [ super.hatchling ];
+      patches = [ ];
+      postPatch = old.postPatch or "" + ''
+        substituteInPlace telethon/version.py --replace-fail \
+          "__version__ = '1.44.0'" "__version__ = '${version}'"
+      '';
+      nativeCheckInputs = [ ];
+    }
+  );
 }
